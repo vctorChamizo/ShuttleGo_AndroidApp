36121aebad520d662b4677feb2634d88373bcf13
carlosCharlie <32466601+carlosCharlie@users.noreply.github.com>
2018-11-04 21:08:34 +0100

    organizacion

--- Shuttle/app/src/main/java/es/tfg/shuttle/MainActivity.java
+++ Shuttle/app/src/main/java/es/tfg/shuttle/MainActivity.java
@@ -3,6 +3,8 @@ package es.tfg.shuttle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import es.tfg.shuttle.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
