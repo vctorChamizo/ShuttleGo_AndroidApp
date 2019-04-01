ddf39865ce9349d37878dd5019165ad977dbff95
carlosCharlie <32466601+carlosCharlie@users.noreply.github.com>
2019-04-01 21:07:29 +0200

    error controlado

--- ShuttleGo/app/src/main/java/tfg/shuttlego/activities/route/RouteCalculate.java
+++ ShuttleGo/app/src/main/java/tfg/shuttlego/activities/route/RouteCalculate.java
@@ -325,6 +325,7 @@ public class RouteCalculate extends AppCompatActivity implements OnMapReadyCallb
     public void onClick(View v) {
         if (!this.started) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                throwToast(R.string.cantAccessToLocation);
                 return;
             }
 
