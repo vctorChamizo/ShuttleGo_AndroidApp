280f4321a25726cc7cf85ab632256fd35fcfb38d
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2019-04-01 15:44:54 +0200

    Registro casi completado

--- ShuttleGo/app/src/main/java/tfg/shuttlego/activities/account/LoginMain.java
+++ ShuttleGo/app/src/main/java/tfg/shuttlego/activities/account/LoginMain.java
@@ -3,17 +3,18 @@ package tfg.shuttlego.activities.account;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Objects;
-
 import tfg.shuttlego.R;
 import tfg.shuttlego.activities.person.admin.AdminMain;
 import tfg.shuttlego.activities.person.driver.DriverMain;
@@ -26,11 +27,11 @@ import tfg.shuttlego.model.transfer.person.TypePerson;
 
 public class LoginMain extends AppCompatActivity implements View.OnClickListener {
 
-    private ProgressBar LoginMainProgress;
-    private RelativeLayout LoginMainRelative;
-    private EditText LoginMainTextEmail, LoginMainTextPassword;
-    private Button LoginMainButtonSignin, LoginMainButtonRegister;
-    private Class nextClass;
+    private ProgressBar loginMainProgress;
+    private ScrollView loginMainScroll;
+    private EditText loginMainTextEmail, loginMainTextPassword;
+    private Button loginMainButtonSignin, loginMainButtonRegister;
+    private Class loginMainNextClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,47 +41,55 @@ public class LoginMain extends AppCompatActivity implements View.OnClickListener
 
         inicializateView();
 
-        LoginMainButtonRegister.setOnClickListener(this);
-        LoginMainButtonSignin.setOnClickListener(this);
+        this.loginMainButtonRegister.setOnClickListener(this);
+        this.loginMainButtonSignin .setOnClickListener(this);
     }
 
     /**
-     * Inicializate the componentes of this view
+     * Inicializate the components of this view
      */
     private void inicializateView() {
 
-        LoginMainRelative = findViewById(R.id.main_login_relative);
-        LoginMainProgress = findViewById(R.id.main_login_progress);
-        LoginMainTextEmail = findViewById(R.id.main_login_email);
-        LoginMainTextPassword = findViewById(R.id.main_login_password);
-        LoginMainButtonRegister = findViewById(R.id.main_login_register_btn);
-        LoginMainButtonSignin = findViewById(R.id.main_login_signin_btn);
+        this.loginMainScroll = findViewById(R.id.main_login_scroll);
+        this.loginMainProgress = findViewById(R.id.main_login_progress);
+        this.loginMainTextEmail = findViewById(R.id.main_login_email);
+        this.loginMainTextPassword = findViewById(R.id.main_login_password);
+
+        this.loginMainButtonRegister = findViewById(R.id.main_login_register_btn);
+        this.loginMainButtonSignin = findViewById(R.id.main_login_signin_btn);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
     /**
      * Show the progress bar component visible and put invisble the rest of the view
      */
     private void setProgressBar () {
-        LoginMainProgress.setVisibility(View.VISIBLE);
-        LoginMainRelative.setVisibility(View.GONE);
+        this.loginMainProgress.setVisibility(View.VISIBLE);
+        this.loginMainScroll.setVisibility(View.GONE);
     }
 
     /**
      * Show the view visible and put invisble progress bar component
      */
     private void removeProgressBar () {
-        LoginMainProgress.setVisibility(View.GONE);
-        LoginMainRelative.setVisibility(View.VISIBLE);
+        this.loginMainProgress.setVisibility(View.GONE);
+        this.loginMainScroll.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Build a JSON for to allow make a create the credential to login a user.
+     *
+     * @return JSON with information to login a user.
+     */
     private JSONObject buildJson() {
 
         JSONObject json = new JSONObject();
         JSONObject user = new JSONObject();
 
         try {
-            String email = LoginMainTextEmail.getText().toString();
-            String password = LoginMainTextPassword.getText().toString();
+            String email = this.loginMainTextEmail.getText().toString();
+            String password = this.loginMainTextPassword.getText().toString();
 
             json.put("email", email);
             json.put("password", password);
@@ -94,6 +103,7 @@ public class LoginMain extends AppCompatActivity implements View.OnClickListener
     /**
      * Throw the event that allow to check the user credentials and start the application.
      *
+     * @param data JSON with information to check a login user.
      */
     private void throwEventLoginUser(JSONObject data) {
 
@@ -113,14 +123,14 @@ public class LoginMain extends AppCompatActivity implements View.OnClickListener
                 switch (Objects.requireNonNull(task.getResult().get("error"))) {
                     case "incorrectSignin": throwToast(R.string.errIncorrectSignin); break;
                     case "userDoesntExists": throwToast(R.string.errUserDontExist); break;
-                    case "server": throwToast(R.string.errServer);break;
+                    case "server": throwToast(R.string.errServer); break;
                 }
             }
             else {
 
                 Person user = parserTypePerson(task.getResult());
                 Session.getInstance(getApplicationContext()).setUser(user);
-                startActivity(new Intent(LoginMain.this, nextClass));
+                startActivity(new Intent(LoginMain.this, this.loginMainNextClass));
                 finish();
             }
         });
@@ -130,7 +140,8 @@ public class LoginMain extends AppCompatActivity implements View.OnClickListener
      * Parser the credential to the new user.
      *
      * @param p data to build a user that will use the application
-     * @return the credentials to the curretn user.
+     *
+     * @return  user object whit data to current user.
      */
     private Person parserTypePerson (HashMap<String, ?> p) {
 
@@ -149,17 +160,17 @@ public class LoginMain extends AppCompatActivity implements View.OnClickListener
 
             case "passenger":
                 typePerson = TypePerson.USER;
-                nextClass = PassengerMain.class;
+                this.loginMainNextClass = PassengerMain.class;
                 break;
 
             case "driver":
                 typePerson = TypePerson.DRIVER;
-                nextClass = DriverMain.class;
+                this.loginMainNextClass = DriverMain.class;
                 break;
 
             default:
                 typePerson = TypePerson.ADMIN;
-                nextClass = AdminMain.class;
+                this.loginMainNextClass = AdminMain.class;
                 break;
         }
 
@@ -173,18 +184,23 @@ public class LoginMain extends AppCompatActivity implements View.OnClickListener
 
         switch (v.getId()) {
 
-            case R.id.main_login_register_btn: startActivity(new Intent(LoginMain.this, RegisterActivity.class)); break;
+            case R.id.main_login_register_btn:
+                startActivity(new Intent(LoginMain.this, RegisterMain.class));
+                overridePendingTransition(R.anim.right_in, R.anim.right_out);
+                break;
 
             case R.id.main_login_signin_btn:
 
-                if (LoginMainTextEmail.getText().toString().isEmpty() ||
-                    LoginMainTextPassword.getText().toString().isEmpty()) throwToast(R.string.errDataEmpty);
+                if (this.loginMainTextEmail.getText().toString().isEmpty() ||
+                    this.loginMainTextPassword.getText().toString().isEmpty()) throwToast(R.string.errDataEmpty);
                 else {
-
                     setProgressBar();
                     throwEventLoginUser(buildJson());
                 }
                 break;
         }
     }
+
+    @Override
+    public void onBackPressed() { finish(); }
 }
